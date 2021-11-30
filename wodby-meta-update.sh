dcb7cd562498c76583627adedae091830e85449f
Chingis Sandanov <chingis@wodby.com>
2021-11-30 13:13:07 +0800

    Manually add www-data group

--- wodby-meta-update.sh
+++ wodby-meta-update.sh
@@ -9,6 +9,8 @@ for ver in "${versions[@]}"; do
   # Change basic image.
   sed -i '/FROM alpine/i\ARG BASE_IMAGE_TAG\n' ${ver}/alpine3.15/fpm/Dockerfile.wodby
   sed -i 's/FROM alpine.*/FROM wodby\/alpine:${BASE_IMAGE_TAG}/' ${ver}/alpine3.15/fpm/Dockerfile.wodby
+  # Alpine 3.13 doesn't have www-data group unlike newer versions, so we have to create one manually.
+  sed -i '/adduser /i\    addgroup -g 82 -S www-data; \\' ${ver}/alpine3.15/fpm/Dockerfile.wodby
 
   fullVersion=$(grep -oP '(?<=^ENV PHP_VERSION )([0-9\.]+)' ${ver}/alpine3.15/fpm/Dockerfile.wodby)
   minorVersion=$(echo "${fullVersion}" | grep -oE '^[0-9]+\.[0-9]+')
