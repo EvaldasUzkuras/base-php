52db18ef9be36c2236bf0c850cf55e0ef66b91e7
Chingis Sandanov <chingis@wodby.com>
2022-04-06 21:50:06 +0800

    Update template for Alpine 3.13+

--- wodby-meta-update.sh
+++ wodby-meta-update.sh
@@ -10,7 +10,7 @@ for ver in "${versions[@]}"; do
   sed -i '/FROM alpine/i\ARG BASE_IMAGE_TAG\n' ${ver}/alpine3.15/fpm/Dockerfile.wodby
   sed -i 's/FROM alpine.*/FROM wodby\/alpine:${BASE_IMAGE_TAG}/' ${ver}/alpine3.15/fpm/Dockerfile.wodby
   # Alpine 3.13 doesn't have www-data group unlike newer versions, so we have to create one manually.
-  sed -i '/adduser /i\    addgroup -g 82 -S www-data; \\' ${ver}/alpine3.15/fpm/Dockerfile.wodby
+  sed -i '/adduser /i\    if cat /etc/os-release | grep -q VERSION_ID=3.13; then addgroup -g 82 -S www-data; fi; \\' ${ver}/alpine3.15/fpm/Dockerfile.wodby
 
   fullVersion=$(grep -oP '(?<=^ENV PHP_VERSION )([0-9\.]+)' ${ver}/alpine3.15/fpm/Dockerfile.wodby)
   minorVersion=$(echo "${fullVersion}" | grep -oE '^[0-9]+\.[0-9]+')
